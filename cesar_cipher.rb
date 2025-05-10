53a49599a3a1ecd50038b26d1fa26adbe8363a25
Alex Rider <177359264+abicyclerider@users.noreply.github.com>
2025-05-10 07:41:43 -0600

    Finish basic handling of caps

--- cesar_cipher.rb
+++ cesar_cipher.rb
@@ -7,11 +7,16 @@ def decode_character(number)
   chars[number]
 end
 
-def shift_character(character, shift)
-  (encode_character(character) + shift)%25
+def shift_character(character, shift, num_chars = 26)
+  encoded_char = (encode_character(character) + shift)%num_chars
 
+  unless character.upcase == character
+    return decode_character(encoded_char)
+  else
+    return decode_character(encoded_char).upcase
+  end
 end
 
 def caesar_cipher(string, shift)
-  
+  string.split('').map {|char| shift_character(char, shift)}.join() 
 end
